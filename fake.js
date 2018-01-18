c50b64bc497ce99947181113d96643eedf2a806d
Lamhot Simamora <lamhotsimamora36@gmail.com>
2018-01-18 21:36:46 +0700

    Update fake.js

--- fake.js
+++ fake.js
@@ -2,9 +2,7 @@ function sum(a,b){
    return a+b;
 }
 
-function echo(){
-   alert("Hei ! I am the fake script for development !");
-}
+var _text = 'Hei ! I am the fake script for development !';
 
 function minus(a,b){
   return a-b;
